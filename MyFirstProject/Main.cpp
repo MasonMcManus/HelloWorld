63f02646e125b3e157e6e098d18f8ddeb1917b08
MasonMcManus <MCMANUS2@tritons.iowacentral.edu>
2021-09-05 12:15:25 -0500

    Add project files.

---
+++ MyFirstProject/Main.cpp
@@ -0,0 +1,16 @@
+/*
+MasonMcManus
+C++ Fa21
+Due: September 1st 2021
+CIS162
+MyFirstProject
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Orange" << std::endl;
+	return 0;
+}
